5fc56a07051a371c2a3d5f9c8c4ec7e093fe3610
Scott van den Broek <scottzico2004@gmail.com>
2022-09-22 12:46:08 +0200

    basis layout van de Header gemaakt

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,16 @@
+import "./Header.css"
+import Button from "../Button/Button";
+
 const Header = () => {
     return(
         <header class="header">
-            <h1>Test</h1>
+            <div class="header__container">
+                <h1 class="header__h1">Collezione</h1>
+                <h2 class="header__h2">Een kleine tekst</h2>
+                <Button/>
+            </div>
         </header>
+
     );
 }
 
