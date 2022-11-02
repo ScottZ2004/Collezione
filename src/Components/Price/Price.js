6ff377e4aaba388fc0b22cfea870231a6f9262cc
ScottZ2004 <70643094+ScottZ2004@users.noreply.github.com>
2022-11-02 17:11:59 +0100

    Feature prices (#5)
    * added the best value label
    
    * the checks above the prices are now responsive and get props from the prices component
    
    * progress in the check function
    
    * you can now click a price and the other price(es) will we be unchecked

--- src/Components/Price/Price.js
+++ src/Components/Price/Price.js
@@ -4,40 +4,49 @@ import React from "react";
 class Price extends React.Component{
     constructor(props) {
         super(props);
-        this.className = "price__input";
         this.state = {
-            checked: this.props.checked,
-            best_value: this.props.best_value
+            best_value: this.props.best_value,
+            id: this.props.id
         }
     }
 
     componentDidMount() {
-        this.setState({checked: this.props.checked});
-        this.setState({best_value: this.props.best_value})
+        this.setState({
+            checked: this.props.checked,
+            best_value: this.props.best_value,
+            id: this.props.id
+        });
     }
 
+
     onclick = () => {
-        this.setState({checked: !this.state.checked});
+        this.props.checkPrice(this.state.id);
+        this.setState({checked: this.state.checked});
     }
 
     render(){
-        let svg;
-        if (this.state.checked){
-            this.className = "price__input--checked";
-            this.svg = <svg className="price__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
-                <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#00a452"/>
-            </svg>;
+        let className = null;
+
+        if (this.props.checked){
+            this.svg = (
+                <svg className="price__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
+                    <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#00a452"/>
+                </svg>
+            );
+            className = "price__input--checked";
+
         }else{
-            this.className = "price__input";
-            this.svg = <div></div>
+            this.svg = null;
+            className = "price__input";
         }
 
-        let bestValue;
         if (this.state.best_value){
-            this.bestValue = <p className="price__best_value">Best value</p>;
+            this.bestValue = <p className="price__best_value">Best value</p>
+        }else{
+            this.bestValue = null;
         }
         return(
-            <div onClick={this.onclick} className={this.className}>
+            <div onClick={this.onclick} className={className}>
                 <div>
                     <p>{this.bestValue}</p>
                     <h3 className="price__h3">{this.props.h3}</h3>
--- src/Components/Prices/Prices.js
+++ src/Components/Prices/Prices.js
@@ -1,74 +1,150 @@
 import "./Prices.css";
 import Price from "../Price/Price";
 import React from "react";
+import Check from "../Check/Check";
 
 class Prices extends React.Component{
-    constructor() {
-        super();
-        this.state ={
+    constructor(props) {
+        super(props);
+        this.state = {
+            price_list: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
             price_list: [
                 {
                     'h3': '',
                     'h1': 'Gratis',
                     'h2': '4.83 EUR/month',
-                    'checked': false,
                     'id': 1,
-                    'best-value': false,
+                    'best_value': false,
+                    'checked': false,
                 },
                 {
                     'h3': 'Jaarlijks',
                     'h1': '14 days free',
                     'h2': '4.83 EUR/month',
-                    'checked': true,
                     'id': 2,
-                    'best-value': true,
+                    'best_value': true,
+                    'checked': true,
                 },
                 {
                     'h3': 'Annual',
                     'h1': '14 days free',
                     'h2': '4.83 EUR/month',
-                    'checked': false,
                     'id': 3,
                     'best_value': false,
+                    'checked': false,
                 }
             ]
-        }
+        })
     }
 
+    checkPrice = (id) =>{
+        let TempState;
+        if (id === 1){
+             TempState = [
+                {
+                    'h3': '',
+                    'h1': 'Gratis',
+                    'h2': '4.83 EUR/month',
+                    'id': 1,
+                    'best_value': false,
+                    'checked': true,
+                },
+                {
+                    'h3': 'Jaarlijks',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 2,
+                    'best_value': true,
+                    'checked': false,
+                },
+                {
+                    'h3': 'Annual',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 3,
+                    'best_value': false,
+                    'checked': false,
+                }
+            ];
+        }else if (id === 2){
+             TempState = [
+                {
+                    'h3': '',
+                    'h1': 'Gratis',
+                    'h2': '4.83 EUR/month',
+                    'id': 1,
+                    'best_value': false,
+                    'checked': false,
+                },
+                {
+                    'h3': 'Jaarlijks',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 2,
+                    'best_value': true,
+                    'checked': true,
+                },
+                {
+                    'h3': 'Annual',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 3,
+                    'best_value': false,
+                    'checked': false,
+                }
+            ];
+        }else if (id === 3){
+             TempState = [
+                {
+                    'h3': '',
+                    'h1': 'Gratis',
+                    'h2': '4.83 EUR/month',
+                    'id': 1,
+                    'best_value': false,
+                    'checked': false,
+                },
+                {
+                    'h3': 'Jaarlijks',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 2,
+                    'best_value': true,
+                    'checked': false,
+                },
+                {
+                    'h3': 'Annual',
+                    'h1': '14 days free',
+                    'h2': '4.83 EUR/month',
+                    'id': 3,
+                    'best_value': false,
+                    'checked': true,
+                }
+            ];
+        }
+
+
+        this.setState({price_list: TempState});
+
+    }
 
     render(){
         let prices = this.state.price_list.map(price =>{
-            return <Price best_value={this.best_value} h3={price.h3} h1={price.h1} h2={price.h2} checked={price.checked} key={price.id} id={price.id}/>
+            return <Price checkPrice={this.checkPrice} best_value={price.best_value} h3={price.h3} h1={price.h1} h2={price.h2} key={price.id} checked={price.checked} id={price.id}/>
         })
         return(
             <article id="prijs" className="prices">
                 <header className="prices__header">
                     <h1 className="prices__h1">Probeer Collezione</h1>
                     <div className="prices__check_container">
-                        <div className="prices__check">
-                            <svg className="prices__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
-                                <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#ffa867"/>
-                            </svg>
-                            <p className="prices__check__p">Toegang tot Collezione</p>
-                        </div>
-                        <div className="prices__check">
-                            <svg className="prices__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
-                                <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#ffa867"/>
-                            </svg>
-                            <p className="prices__check__p">Toegang tot Collezione</p>
-                        </div>
-                        <div className="prices__check">
-                            <svg className="prices__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
-                                <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#ffa867"/>
-                            </svg>
-                            <p className="prices__check__p">Toegang tot Collezione</p>
-                        </div>
-                        <div className="prices__check">
-                            <svg className="prices__check__svg" xmlns="http://www.w3.org/2000/svg" width="36" height="26.846" viewBox="0 0 36 26.846">
-                                <path id="Icon_awesome-check" data-name="Icon awesome-check" d="M12.227,30.9.527,19.2a1.8,1.8,0,0,1,0-2.546L3.073,14.1a1.8,1.8,0,0,1,2.546,0L13.5,21.986,30.382,5.1a1.8,1.8,0,0,1,2.546,0L35.473,7.65a1.8,1.8,0,0,1,0,2.546l-20.7,20.7A1.8,1.8,0,0,1,12.227,30.9Z" transform="translate(0 -4.577)" fill="#ffa867"/>
-                            </svg>
-                            <p className="prices__check__p">Toegang tot Collezione</p>
-                        </div>
+                        <Check title="Onbeperkt toegang tot Collezione"/>
+                        <Check title="Altijd je geld terug!"/>
+                        <Check title="Altijd veilig met Collezione"/>
+                        <Check title="Kom in contact met andere achtbaan liefhebbers!"/>
                     </div>
                 </header>
                 <section className="prices__inputWrapper">
