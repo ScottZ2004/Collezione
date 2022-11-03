24dd560ce3a5e555f521d09da403473d309446d8
Scott van den Broek <scottzico2004@gmail.com>
2022-11-03 09:36:14 +0100

    the drop up in the footer is now standard down

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            dropDownIsActive: true,
+            dropDownIsActive: false,
             language: "Nederlands",
         }
     }
