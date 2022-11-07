e52f9c9efcff10fc2491919ef1ec32e274ffd2cf
Scott van den Broek <scottzico2004@gmail.com>
2022-11-07 17:32:45 +0100

    created the login component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Homepage from "../Components/Homepage/Homepage";
+import Login from "../Components/Login/Login"
 import{BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const App = () =>{
     return(
         <Router>
             <Switch>
-
+                <Route path="/login/">
+                    <Login/>
+                </Route>
+                <Route path="/">
+                    <Homepage/>
+                </Route>
             </Switch>
         </Router>
     );
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,9 @@
+import "./Login.css";
+
+const Login = () => {
+    return(
+        <h1>ik ben Login</h1>
+    );
+}
+
+export default Login;
