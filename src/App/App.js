fad31f40f1537a17b8564cd58883c192e218033d
Scott van den Broek <scottzico2004@gmail.com>
2022-11-08 18:37:23 +0100

    added the signup component

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import Homepage from "../Components/Homepage/Homepage";
 import Login from "../Components/Login/Login";
 import usersList from "../data/users";
 import{BrowserRouter as Router, Route, Switch, useHistory} from "react-router-dom";
+import Signup from "../Components/Signup/Signup";
 
 class App extends React.Component{
     constructor(props) {
@@ -23,6 +24,9 @@ class App extends React.Component{
                 <Switch>
                     <Route path="/login">
                         <Login users={this.state.users}/>
+                    </Route>
+                    <Route path="/signup">
+                        <Signup/>
                     </Route>
                     <Route path="/dashboard">
                         <h1>Hallo daar</h1>
