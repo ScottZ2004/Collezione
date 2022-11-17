65f27236ffd323146a1a29b10a8a0603aa89d37e
ScottZ2004 <70643094+ScottZ2004@users.noreply.github.com>
2022-11-17 19:37:27 +0100

    Feature edit item (#11)
    * if you click on the edit button, the item information becomes a form
    
    * you can now change and save the items in the collection
    
    * the from is now styled

--- src/Components/Collection/CollectionLeft/CollectionLeft.js
+++ src/Components/Collection/CollectionLeft/CollectionLeft.js
@@ -5,8 +5,105 @@ import {BsFillPencilFill} from "react-icons/bs";
 class CollectionLeft extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            title: this.props.item.title,
+            description: this.props.item.description,
+            build_year: this.props.item.Build_Year,
+            park: this.props.item.Park,
+        }
     }
+
+    componentDidMount() {
+        this.setState({
+            title: this.props.item.title,
+            description: this.props.item.description,
+            build_year: this.props.item.Build_Year,
+            park: this.props.item.Park,
+        })
+    }
+
+    changeMode = () => {
+        this.props.changeMode();
+    }
+
+    onInputchange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    onButtonClick = () => {
+        let title = this.state.title;
+        if (this.state.title === null || this.state.title === undefined){
+            title = this.props.item.title;
+        }
+        let description = this.state.description;
+        if (this.state.description === null || this.state.description === undefined){
+            description = this.props.item.description;
+        }
+        let build_year = this.state.build_year;
+        if (this.state.build_year === null || this.state.build_year === undefined){
+            build_year = this.props.item.Build_Year;
+        }
+        let park = this.state.park;
+        if (this.state.park === null || this.state.park === undefined){
+            park = this.props.item.Park;
+        }
+        this.props.saveItem(title, description, build_year, park)
+    }
+
     render(){
+        let articleSection = (
+            <article className="collection__selected__article">
+                <h1 className="collection__selected__h1">{this.props.item.title || "title"}</h1>
+                <p className="collection__selected__p">{this.props.item.description || "description"}</p>
+                <h2 className="collection__selected__h2">Bouw jaar: {this.props.item.Build_Year || "0000"}</h2>
+                <h2 className="collection__selected__h2">Park: {this.props.item.Park || "naam park"}</h2>
+            </article>
+        );
+
+        if (this.props.editMode === true){
+            articleSection = (
+                <article className="collection__selected__article">
+                    <label className="collection__selected__label" htmlFor="title">Achtbaan naam</label>
+                    <input
+                        placeholder="hallo"
+                        className="collection__selected__input"
+                        type="text"
+                        name="title"
+                        id="title"
+                        defaultValue={this.props.item.title}
+                        onChange={this.onInputchange}/>
+                    <label className="collection__selected__label" htmlFor="description">Achtbaan descriptie</label>
+                    <textarea
+                        placeholder="hallo"
+                        className="collection__selected__textarea"
+                        name="description" id="description"
+                        defaultValue={this.props.item.description}
+                        onChange={this.onInputchange}></textarea>
+                    <label className="collection__selected__label" htmlFor="build_year">Achtbaan bouw jaar</label>
+                    <input
+                        placeholder="hallo"
+                        className="collection__selected__input"
+                        type="text"
+                        name="build_year"
+                        id="build_year"
+                        defaultValue={this.props.item.Build_Year}
+                        onChange={this.onInputchange}/>
+                    <label className="collection__selected__label" htmlFor="park">Achtbaan park</label>
+                    <input
+                        placeholder="hallo"
+                        className="collection__selected__input"
+                        type="text"
+                        name="park"
+                        id="park"
+                        defaultValue={this.props.item.Park}
+                        onChange={this.onInputchange}/>
+                    <button onClick={this.onButtonClick} className="collection__selected__button">Opslaan</button>
+                </article>
+            )
+        }
+
         return(
             <div className="collection__Left">
                 <header className="collection__heading">
@@ -17,16 +114,11 @@ class CollectionLeft extends React.Component{
                     <img className="collection__selected__image" src={this.props.item.img || Poseidon} alt={this.props.item.title || "achtbaan heeft geen eigen afbeelding"}/>
                 </div>
                 <div className="collection__selected__svgContainer">
-                    <div className="collection__selected__svg">
+                    <div onClick={this.changeMode} className="collection__selected__svg">
                         <BsFillPencilFill/>
                     </div>
                 </div>
-                <article className="collection__selected__article">
-                    <h1 className="collection__selected__h1">{this.props.item.title || "title"}</h1>
-                    <p className="collection__selected__p">{this.props.item.description || "description"}</p>
-                    <h2 className="collection__selected__h2">Bouw jaar: {this.props.item.Build_Year || "0000"}</h2>
-                    <h2 className="collection__selected__h2">Park: {this.props.item.Park || "naam park"}</h2>
-                </article>
+                {articleSection}
             </div>
         )
     }
