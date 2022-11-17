2e4399d258daebd70196debf051c020992ff5e71
Scott van den Broek <scottzico2004@gmail.com>
2022-11-17 15:27:54 +0100

    if you click on the edit button, the item information becomes a form

--- src/Components/Collection/CollectionLeft/CollectionLeft.js
+++ src/Components/Collection/CollectionLeft/CollectionLeft.js
@@ -5,8 +5,41 @@ import {BsFillPencilFill} from "react-icons/bs";
 class CollectionLeft extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            editMode: true,
+        }
     }
+
+    changeMode = () => {
+        this.setState({editMode: !this.state.editMode})
+    }
+
     render(){
+        let articleSection = (
+            <article className="collection__selected__article">
+                <h1 className="collection__selected__h1">{this.props.item.title || "title"}</h1>
+                <p className="collection__selected__p">{this.props.item.description || "description"}</p>
+                <h2 className="collection__selected__h2">Bouw jaar: {this.props.item.Build_Year || "0000"}</h2>
+                <h2 className="collection__selected__h2">Park: {this.props.item.Park || "naam park"}</h2>
+            </article>
+        );
+
+        if (this.state.editMode === true){
+            articleSection = (
+                <article className="collection__selected__article">
+                    <label className="collection__selected__label" htmlFor="title">Achtbaan naam</label>
+                    <input className="collection__selected__input" type="text" id="title" placeholder={this.props.item.title}/>
+                    <label className="collection__selected__label" htmlFor="title">Achtbaan descriptie</label>
+                    <textarea className="collection__selected__textarea" name="" id="" value={this.props.item.description} cols="30" rows="10"></textarea>
+                    <label className="collection__selected__label" htmlFor="title">Achtbaan bouw jaar</label>
+                    <input className="collection__selected__input" type="text" placeholder={this.props.item.Build_Year}/>
+                    <label className="collection__selected__label" htmlFor="title">Achtbaan park</label>
+                    <input className="collection__selected__input" type="text" placeholder={this.props.item.Park}/>
+                    <button className="collection__selected__button">Opslaan</button>
+                </article>
+            )
+        }
+
         return(
             <div className="collection__Left">
                 <header className="collection__heading">
@@ -17,16 +50,11 @@ class CollectionLeft extends React.Component{
                     <img className="collection__selected__image" src={this.props.item.img || Poseidon} alt={this.props.item.title || "achtbaan heeft geen eigen afbeelding"}/>
                 </div>
                 <div className="collection__selected__svgContainer">
-                    <div className="collection__selected__svg">
+                    <div onClick={this.changeMode} className="collection__selected__svg">
                         <BsFillPencilFill/>
                     </div>
                 </div>
-                <article className="collection__selected__article">
-                    <h1 className="collection__selected__h1">{this.props.item.title || "title"}</h1>
-                    <p className="collection__selected__p">{this.props.item.description || "description"}</p>
-                    <h2 className="collection__selected__h2">Bouw jaar: {this.props.item.Build_Year || "0000"}</h2>
-                    <h2 className="collection__selected__h2">Park: {this.props.item.Park || "naam park"}</h2>
-                </article>
+                {articleSection}
             </div>
         )
     }
