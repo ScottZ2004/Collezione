eafb748db01d66e9282c6a877ee2a921a5c6e4e2
ScottZ2004 <70643094+ScottZ2004@users.noreply.github.com>
2023-01-12 16:58:08 +0100

    Feature navigation fix (#18)
    * refactored the navigation component
    
    * the link types are now all correct
    
    * fixed a bug that prevented opening the hamburger menu if there was nothing in the navigation bar
    
    * made navigation items for the backlog page

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,89 +1,101 @@
 import logo from '../../images/logo.png';
 import "./Navigation.css";
-import Button from "../Button/Button";
+import CollectionContext from '../../Context/CollectionContext';
 import React  from "react";
 import {GiHamburgerMenu} from 'react-icons/gi';
 import {ImCross} from 'react-icons/im';
 import {AiOutlineArrowRight} from 'react-icons/ai';
 import {Link} from "react-router-dom";
+import { useState,useContext } from 'react';
 
 
-class Navigation extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            hamburgermenu: true,
-            showSideNavigation: false,
-        }
+const Navigation = (props) =>{
+    const {user} = useContext(CollectionContext);
+    const [hamburgerMenuIsOn, setHamburgerMenuIsOn] = useState(true)
+    const [showSideNavigation, setShowSideNavigation] = useState(false)    
+
+    const onClickHamburger = () => {
+        setHamburgerMenuIsOn(!hamburgerMenuIsOn);
+        setShowSideNavigation(!showSideNavigation);
     }
 
-    onClickHamburger = () => {
-        this.setState({hamburgermenu: !this.state.hamburgermenu, showSideNavigation: !this.state.showSideNavigation})
+    const url = window.location.pathname;
+    let leftButton;
+    if (url === "/login"){
+        leftButton = (<div className="navigation__right">
+            <Link to="/signup" className="button">Signup</Link>
+        </div>)
+    }else if (!user.isLoggedIn){
+        leftButton = (<div className="navigation__right">
+            <Link to="/login" className="button">Login</Link>
+        </div>)
+    }else if( url === "/"){
+        let goTo = "/user/" + user.userId + "/collection/"
+        leftButton = (<div className="navigation__right">
+            <Link to={goTo} className="button">Collectie</Link>
+        </div>)
+    }else{
+        leftButton = (<div className="navigation__right">
+            <Link to="/delen" className="button">Delen</Link>
+        </div>) 
     }
 
-    render(){
-        const url = window.location.pathname;
-        let leftButton;
-        if (url === "/login"){
-            leftButton = (<div className="navigation__right">
-                <Link to="/signup" className="button">Signup</Link>
-            </div>)
-        }else{
-            leftButton = (<div className="navigation__right">
-                <Link to="/login" className="button">Login</Link>
-            </div>)
-        }
-
-        let items = null;
-        if (this.props.items != null){
-            items = this.props.items.map(item => {
+    let items = null;
+    if (props.items != null){
+        items = props.items.map(item => {
+            if(item.goto[0] === "#"){
                 return <a key={item.name} className={`navigation__item navigation__item--${item.border_color} `} href={item.goto}>{item.name}</a>
-            });
-        }
-
-
-        let hamburgerMenu = null;
-        if (this.state.hamburgermenu){
-            hamburgerMenu = (<GiHamburgerMenu onClick={this.onClickHamburger} className="navigation__icon"/>)
-        }else if(!this.state.hamburgermenu){
-            hamburgerMenu = (<ImCross onClick={this.onClickHamburger} className="navigation__icon"/>)
-        }
-
-        let sideNavigation = null;
-        let sideNavigationItems = null;
-        if (this.state.showSideNavigation){
-            sideNavigationItems = this.props.items.map(item => {
-                return <a className="sideNavigation__item" key={item.name} href={item.goto}><li>{item.name}</li><AiOutlineArrowRight/></a>
-            })
-            sideNavigation = (
-                <ul className="sideNavigation">
-                    {sideNavigationItems}
-                </ul>
-            );
-        }else if(!this.state.showSideNavigation){
-            sideNavigation = null;
-        }
-
-        return(
-            <>
-                <nav className="navigation">
-                    <div className="navigation__left">
-                        {hamburgerMenu}
-                        <Link className="navigation__logo" to="/">
-                            <img src={logo} alt="logo" className="navigation__img"/>
-                            <h1 className="navigation__logo__text">Collezione</h1>
-                        </Link>
-                        <ul className="navigation__list">
-                            {items}
-                        </ul>
-                    </div>
-                    {leftButton}
-                </nav>
-                {sideNavigation}
-            </>
-
-        );
+            }
+            return <Link key={item.name} className={`navigation__item navigation__item--${item.border_color} `} to={item.goto}>{item.name}</Link>
+            
+        });
     }
+
+    let hamburgerMenu = null;
+    if (hamburgerMenuIsOn){
+        hamburgerMenu = (<GiHamburgerMenu onClick={onClickHamburger} className="navigation__icon"/>)
+    }else if(!hamburgerMenuIsOn){
+        hamburgerMenu = (<ImCross onClick={onClickHamburger} className="navigation__icon"/>)
+    }
+
+    let sideNavigation = null;
+    if (showSideNavigation && props.items != null){
+        let sideNavigationItems = null;
+        sideNavigationItems = props.items.map(item => {
+            if(item.goto[0] === "#"){
+                return <a className="sideNavigation__item" key={item.name} href={item.goto}><li>{item.name}</li><AiOutlineArrowRight/></a>
+            }
+            return <Link className="sideNavigation__item" key={item.name} to={item.goto}><li>{item.name}</li><AiOutlineArrowRight/></Link>
+        })
+        sideNavigation = (
+            <ul className="sideNavigation">
+                {sideNavigationItems}
+            </ul>
+        );
+    }else{
+        sideNavigation = null;
+    }
+
+    return(
+        <>
+            <nav className="navigation">
+                <div className="navigation__left">
+                    {hamburgerMenu}
+                    <Link className="navigation__logo" to="/">
+                        <img src={logo} alt="logo" className="navigation__img"/>
+                        <h1 className="navigation__logo__text">Collezione</h1>
+                    </Link>
+                    <ul className="navigation__list">
+                        {items}
+                    </ul>
+                </div>
+                {leftButton}
+            </nav>
+            {sideNavigation}
+        </>
+
+    );
+    
 }
 
 export default Navigation;
