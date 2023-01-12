ca4e190242c9d76abeb5811c035fcd72758114fd
Scott van den Broek <scottzico2004@gmail.com>
2023-01-12 18:56:23 +0100

    if you click on the copy button, it will copy and the copied text appears

--- src/Components/Share/Share.js
+++ src/Components/Share/Share.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./Share.css"
 
 const Share = () => {
+    const [isCopied, setIsCopied] = useState(false)
+    const copyLink = () => {
+        navigator.clipboard.writeText("");
+        setIsCopied(true)
+    }
+
+    let isCopiedText = null;
+    if(isCopied){
+        isCopiedText = (<p className="share__succes">Link gekopieerd!</p>)
+    }
     return(
         <section className="share__background">
             <article className="share">
                 <h1 className="share__title">Deel deze collectie!</h1>
                 <p className="share__p">Kopieer de onderstaande link om deze collectie met je vrienden met je vriendenen/of famillie te delen</p>
                 <h2 className="share__link">https://collezione/user/1/collection</h2>
-                <button className="button">Kopieer link</button>
-                <p className="share__succes">Link gekopieerd!</p>
+                <button onClick={copyLink} className="button">Kopieer link</button>
+                {isCopiedText}
             </article>
         </section>
     )
